0d3dcc54d5754286056b34b1ae75849a21742826
coldwindx <15653913658@163.com>
2025-04-09 07:56:26 +0000

    全局异常管理+返回值切片

--- Server/src/main/java/com/coldwindx/server/controller/MemoryController.java
+++ Server/src/main/java/com/coldwindx/server/controller/MemoryController.java
@@ -1,9 +1,9 @@
 package com.coldwindx.server.controller;
 
 import com.coldwindx.server.entity.form.Memory;
-import com.coldwindx.server.entity.form.RestResult;
 import com.coldwindx.server.service.MemoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +14,7 @@ public class MemoryController {
     @Autowired
     private MemoryService memoryService;
     @RequestMapping(value = "insert", method = RequestMethod.POST)
-    public RestResult<Memory> insert(Memory memory){
-        Memory m = memoryService.insert(memory);
-        return new RestResult<>(m);
+    public Memory insert(@RequestBody Memory memory){
+        return memoryService.insert(memory);
     }
 }
--- Server/src/main/java/com/coldwindx/server/entity/form/RestResult.java
+++ Server/src/main/java/com/coldwindx/server/entity/form/RestResult.java
@@ -6,9 +6,14 @@ import lombok.Data;
 public class RestResult<T> {
     public Integer code = 0;
     public String message = "success";
-    public T data;
+    public T data = null;
 
     public RestResult(T data){
         this.data = data;
+    }
+
+    public RestResult(int code, String message){
+        this.code = code;
+        this.message = message;
     }
 }
