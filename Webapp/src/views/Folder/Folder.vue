2f68b8e831e66049061bd19accfa3e17d5faca40
coldwindx <15653913658@163.com>
2025-04-14 10:40:49 +0800

    基础文件上传

--- Webapp/src/views/Folder/Folder.vue
+++ Webapp/src/views/Folder/Folder.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="div-upload-page">
-        <el-upload class="upload-demo upload" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15">
+        <el-upload class="upload-demo upload" drag action="/api/minio/upload">
             <el-icon class="el-icon--upload"><upload-filled /></el-icon>
             <div class="el-upload__text">拖拽至此 或 <em>点击上传</em></div>
             <template #tip>
@@ -9,7 +9,7 @@
         </el-upload>
     </div>
 </template>
-  
+
 <script setup lang="ts">
 import { UploadFilled } from '@element-plus/icons-vue'
 </script>
