5b6b08736e2c8e9b0fa1a16e71a5e8a0750158e5
coldwindx <15653913658@163.com>
2025-04-22 10:48:54 +0800

    树形控件风格

--- Webapp/src/views/Contents/Contents.vue
+++ Webapp/src/views/Contents/Contents.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="div-tree-container">
-        <el-tree class="tree" :data="data" :props="props" :height="800" @node-click="onclick"/>
+        <el-tree class="tree" :data="data" :height="800" @node-click="onclick">
+            <template #default="{ node, data }">
+                <!-- 自定义节点内容 -->
+                <span :class="setNodeClass(data)">
+                    {{ data.label }}
+                </span>
+            </template>
+        </el-tree>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
+import type Node from 'element-plus/es/components/tree/src/model/node'
+import type { TreeNodeData } from 'element-plus/es/components/tree/src/tree.type'
 import { SettingApi } from "@/apis/SettingApi";
 
 interface TreeNode {
@@ -16,20 +25,13 @@ interface TreeNode {
     url: string;
     children?: TreeNode[];
 }
-
-const props = {
-    value: 'id',
-    label: 'label',
-    children: 'children',
-}
-
 const data = ref<TreeNode[]>([])
 
 onMounted(() => {
     SettingApi.query({ "condition": { "key": "VUE_CONTENT_NODE" } }).then((res) => {
         let treenodes = res.map(item => JSON.parse(item.value) as TreeNode)
         treenodes.sort((a, b) => a.level - b.level);
-        
+
         let hashnodes = ref<Record<number, TreeNode>>({})
         treenodes.forEach(item => {
             hashnodes.value[item.id] = item
@@ -39,16 +41,19 @@ onMounted(() => {
             }
 
             let parent = hashnodes.value[item.parent]
-            if(parent.children == undefined)
+            if (parent.children == undefined)
                 parent.children = []
             parent.children.push(item)
         });
     })
 })
 
+// todo: 这里定义树节点点击后的跳转事件
 const onclick = (node: TreeNode) => {
     console.log(node)
 }
+
+const setNodeClass = (data) => data.parent == 0 ? "node-root" : "node-child"
 </script>
 
 <style lang="scss" scoped>
@@ -62,5 +67,17 @@ const onclick = (node: TreeNode) => {
     height: 100%;
     overflow: hidden;
     // background-color: black;
+}
+
+.node-root {
+    font-size: 18px;
+    font-weight: bold;
+    color: black;
+}
+
+.node-child{
+    font-size: 16px;
+    color: black;
+    cursor: pointer;
 }
 </style>
