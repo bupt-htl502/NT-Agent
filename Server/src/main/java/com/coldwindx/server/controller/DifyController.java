b2a3254cb9cae465256b11cf03d0abf8326e8d68
coldwindx <15653913658@163.com>
2025-04-25 11:16:02 +0800

    开发Dify对话接口

--- Server/src/main/java/com/coldwindx/server/controller/DifyController.java
+++ Server/src/main/java/com/coldwindx/server/controller/DifyController.java
@@ -1,16 +1,18 @@
 package com.coldwindx.server.controller;
 
 import com.coldwindx.server.aop.UnifiedResponse;
+import com.coldwindx.server.entity.NtAgentException;
+import com.coldwindx.server.entity.enums.ResponseCode;
 import com.coldwindx.server.service.DifyService;
 import io.github.imfangs.dify.client.model.chat.ChatMessageResponse;
+import io.github.imfangs.dify.client.model.file.FileUploadResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
 @UnifiedResponse
 @RestController
@@ -19,8 +21,19 @@ public class DifyController {
     @Autowired
     private DifyService service;
 
+    @RequestMapping(value = "upload", method = RequestMethod.POST)
+    public FileUploadResponse upload(@RequestParam("file") MultipartFile pcap) throws IOException {
+        return service.upload(pcap);
+    }
+
     @RequestMapping(value = "chat", method = RequestMethod.POST)
-    public ChatMessageResponse chat(@RequestParam("file") MultipartFile file, @RequestParam(value = "query") String query) throws IOException {
-        return service.chat(query, file);
+    public ChatMessageResponse chat(@RequestBody Map<String, String> params) throws IOException {
+        String fileid = params.getOrDefault("fileid", "");
+        String query = params.getOrDefault("query", "");
+        if(StringUtils.isBlank(fileid))
+            throw new NtAgentException(ResponseCode.INVALID_PARAMETERS.getCode(), "参数非法：`fileid`不能为空！");
+        if(StringUtils.isBlank(query))
+            throw new NtAgentException(ResponseCode.INVALID_PARAMETERS.getCode(), "参数非法：`query`不能为空！");
+        return service.chat(query, fileid);
     }
 }
--- Server/src/main/java/com/coldwindx/server/service/DifyService.java
+++ Server/src/main/java/com/coldwindx/server/service/DifyService.java
@@ -1,10 +1,12 @@
 package com.coldwindx.server.service;
 
 import io.github.imfangs.dify.client.model.chat.ChatMessageResponse;
+import io.github.imfangs.dify.client.model.file.FileUploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface DifyService {
-    ChatMessageResponse chat(String query, MultipartFile pcap) throws IOException;
+    FileUploadResponse upload(MultipartFile pcap) throws IOException;
+    ChatMessageResponse chat(String query, String fileid) throws IOException;
 }
