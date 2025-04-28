1494d6ffaafa67cd8cf9b8248211cf886c9bb51e
nefelibataaa <1148168540@qq.com>
2025-04-28 16:30:00 +0800

    t_student接口实现增删改查

---
+++ Server/src/main/java/com/coldwindx/server/service/impl/StudentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.coldwindx.server.service.impl;
+
+import com.coldwindx.server.entity.QueryParam;
+import com.coldwindx.server.entity.form.Student;
+import com.coldwindx.server.mapper.StudentMapper;
+import com.coldwindx.server.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentMapper studentMapper;
+    @Override
+    public List<Student> query(QueryParam<Student> params) {
+        return studentMapper.query(params);
+    }
+
+    @Override
+    public Student insert(Student student) {
+        studentMapper.insert(student);
+        return student;
+    }
+
+    @Override
+    public Student update(Student student) {
+        studentMapper.update(student);
+        return student;
+    }
+
+    @Override
+    public int delete(Student student) {
+        return studentMapper.delete(student);
+    }
+}
