947ddf45faca582038bcfbc4347278035df6c6fb
coldwindx <15653913658@163.com>
2025-04-28 16:07:02 +0800

    动态bean获取

--- Server/src/main/java/com/coldwindx/server/service/impl/StaticFeatureEvaluationServiceImpl.java
+++ Server/src/main/java/com/coldwindx/server/service/impl/StaticFeatureEvaluationServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-@Service()
+@Service("staticFeatureEvaluationService")
 public class StaticFeatureEvaluationServiceImpl extends EffectEvaluationService {
 
     @Override
