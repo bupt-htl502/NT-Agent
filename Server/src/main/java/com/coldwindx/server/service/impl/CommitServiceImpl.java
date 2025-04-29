317268432194cb7e8248d9db138ae6b94d2e16fc
nefelibataaa <1148168540@qq.com>
2025-04-29 09:21:06 +0800

    t_commit接口实现增和查询

--- Server/src/main/java/com/coldwindx/server/service/impl/CommitServiceImpl.java
+++ Server/src/main/java/com/coldwindx/server/service/impl/CommitServiceImpl.java
@@ -1,16 +1,24 @@
 package com.coldwindx.server.service.impl;
 
+import com.coldwindx.server.entity.QueryParam;
 import com.coldwindx.server.entity.form.Commit;
-import com.coldwindx.server.entity.form.Student;
 import com.coldwindx.server.mapper.CommitMapper;
 import com.coldwindx.server.service.CommitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CommitServiceImpl implements CommitService {
     @Autowired
     private CommitMapper commitMapper;
+
+    @Override
+    public List<Commit> query(QueryParam<Commit> params) {
+        return commitMapper.query(params);
+    }
+
     @Override
     public Commit insert(Commit commit) {
         commitMapper.insert(commit);
