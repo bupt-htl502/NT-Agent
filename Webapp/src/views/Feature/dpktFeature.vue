3615b0dc2d2f776b8c336b341935fa0a487076bc
nefelibataaa <1148168540@qq.com>
2025-05-12 11:52:55 +0800

    111

--- Webapp/src/views/Feature/dpktFeature.vue
+++ Webapp/src/views/Feature/dpktFeature.vue
@@ -3,56 +3,25 @@
         <span class="experiment-title">{{ props.title }}</span>
         <div class="experiment-body">
             <div class="experiment-page">
-                <el-table :data="tableData" class="experiment-table">
-                    <el-table-column prop="name" label="特征字段" width="300">
-                        <template #header>
-                            <span style="color: black;">特征字段</span>
-                        </template>
-                        <template #default="scope">
-                            <span>{{ scope.row.name }}</span>
-                            <el-tooltip :disabled="disable" class="box-item" effect="dark" content="你必须首先上传pcap文件！"
-                                placement="right-start">
-                                <el-button style="border-style: none;" size="small" :disabled="!disable"
-                                    @click="onClick(scope.$index, scope.row)">
-                                    <svg t="1745544891000" class="icon" viewBox="0 0 1024 1024" version="1.1"
-                                        xmlns="http://www.w3.org/2000/svg" p-id="16587" width="32" height="32">
-                                        <path
-                                            d="M220.11215 950.629283c-9.570093 0-19.140187-3.190031-25.52025-6.380062-22.330218-9.570093-35.090343-31.900312-35.090342-57.420561v-159.501557H118.031153c-57.420561 0-102.080997-44.660436-102.080997-102.080997V185.021807C15.950156 127.601246 60.610592 82.94081 118.031153 82.94081h714.566978c57.420561 0 102.080997 44.660436 102.080997 102.080997V414.70405c0 19.140187-15.950156 35.090343-35.090343 35.090343s-35.090343-15.950156-35.090343-35.090343V185.021807c0-19.140187-15.950156-35.090343-35.090342-35.090343H118.031153c-19.140187 0-35.090343 15.950156-35.090343 35.090343v443.41433c0 19.140187 15.950156 35.090343 35.090343 35.090343h108.461059v213.732087l236.062305-216.922118h51.040499c19.140187 0 35.090343 15.950156 35.090342 35.090342s-15.950156 35.090343-35.090342 35.090343h-25.52025l-226.492211 207.352025c-12.760125 6.380062-25.520249 12.760125-41.470405 12.760124z"
-                                            p-id="16588" fill="#13227a"></path>
-                                        <path
-                                            d="M784.747664 886.82866c-121.221184 0-220.11215-98.890966-220.11215-220.112149s98.890966-220.11215 220.11215-220.11215 220.11215 98.890966 220.112149 220.11215-98.890966 220.11215-220.112149 220.112149z m0-376.423676c-82.94081 0-153.121495 70.180685-153.121496 153.121496s70.180685 153.121495 153.121496 153.121495 153.121495-70.180685 153.121495-153.121495-66.990654-153.121495-153.121495-153.121496z"
-                                            p-id="16589" fill="#13227a"></path>
-                                        <path
-                                            d="M781.557632 759.227414m-19.140187 0a19.140187 19.140187 0 1 0 38.280374 0 19.140187 19.140187 0 1 0-38.280374 0Z"
-                                            p-id="16590" fill="#13227a"></path>
-                                        <path
-                                            d="M781.557632 708.186916c-12.760125 0-19.140187-9.570093-19.140187-19.140187V574.205607c0-12.760125 9.570093-19.140187 19.140187-19.140186 12.760125 0 19.140187 9.570093 19.140187 19.140186v111.651091c0 12.760125-9.570093 22.330218-19.140187 22.330218z"
-                                            p-id="16591" fill="#13227a"></path>
-                                    </svg>
-                                </el-button>
-                            </el-tooltip>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="label" label="特征名" width="200">
-                        <template #header><span style="color: black;">特征名</span></template>
-                    </el-table-column>
+              <div class="feishu">
+                <FeishuDocument :url="documentUrl" />
+              </div>
 
-                    <el-table-column prop="description" label="备注">
-                        <template #header><span style="color: black;">备注</span></template>
-                    </el-table-column>
-
-                </el-table>
-
-                <div class="experiment-download-upload">
+                <div class="experiment-download">
                     <label class="experiment-download-label">注册你的信息</label>
                     <el-button type="primary" @click="register">注册</el-button>
                     <label class="experiment-download-label">下载你的作业</label>
                     <el-button type="primary" @click="downPcap">下载pcap</el-button>
-                    <label class="experiment-upload-label">上传你的pcap试试吧</label>
-                    <el-upload v-model:file-list="pcapfiles" class="upload-demo experiment-upload-btn" accept=".pcap"
-                        action="/api/dify/upload" :on-success="onSuccess" :on-remove="onRemove" :limit="1">
-                        <el-button size="small" type="warning" round>上传pcap</el-button>
-                    </el-upload>
+                    <el-button type="primary" @click="question">有问题请点这里</el-button>
+                    <el-dialog
+                        title="展示页面"
+                        v-model="dialogVisible"
+                        width="50%"
+                        :before-close="handleClose"
+                    >
+                      <!-- 在对话框中嵌入 dpktFeatureDify 组件 -->
+                      <dpktFeatureDify />
+                    </el-dialog>
                 </div>
 
                 <div class="experiment-upload-csv">
@@ -88,57 +57,48 @@
             </div>
         </div>
     </div>
-
-    <el-dialog v-model="resultDialogVisible" :modal="false" :destroy-on-close="true">
-        <template #header="{ titleId, titleClass }">
-            <h4 :id="titleId" :class="titleClass">特征信息</h4>
-        </template>
-        <FeatureResultDialog :fileid="fileid" :feature="feature"></FeatureResultDialog>
-    </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useDifyStore } from "@/store/index";
-import { SettingApi } from "@/apis/SettingApi";
 import { ScoreApi } from "@/apis/ScoreApi";
 import { StudentApi  } from "@/apis/StudentApi";
 import { Student2ResourceApi } from "@/apis/Student2ResourceApi";
-import FeatureResultDialog from "./FeatureResultDialog.vue";
-import { UploadFile, UploadFiles } from "element-plus";
+import {ElMessageBox, UploadFile, UploadFiles} from "element-plus";
 import { ElMessage, ElLoading } from 'element-plus';
 import axios from "axios";
+import FeishuDocument from "@/views/Components/FeishuDocument.vue";
+import dpktFeatureDify from "./dpktFeatureDify.vue";
 
+const documentUrl = ref("https://yu5fu9ktnt.feishu.cn/docx/Ig9ydc1iro9H5lxQlvycQbQFnBd?from=from_copylink");
 const store = useDifyStore();
 const { agent_end_point } = storeToRefs(store);
 const props = defineProps(["title"])
-const tableData = ref([])
-
-onMounted(() => {
-    SettingApi.query({ "condition": { "key": "VUE_TRAFFIC_STATISTICS_FEATURE_FIELD" } })
-        .then((res: any) => {
-            tableData.value = res.map((item: any) => JSON.parse(item.value))
-        })
-})
 
 // 注册
 class Student {
   constructor(public id: number, public name: string, public studentNo: string, public role: number, public grade: number , public isdeleted: boolean) {}
 }
+
+// 设置 cookie 的辅助函数
+function setCookie(name: string, value: string, days: number = 365) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()};path=/`;
+}
+
+const studentId = ref(0)
 const register = async () =>{
-  const student = new Student(0,"gzy","2023145465", 100, 0, false)
-  //todo 学生注册信息时使用cookie缓存学生姓名跟学号
+  const student = new Student(0,"xyq","2023140634", 100, 0, false)
+  setCookie("studentName", student.name);
+  setCookie("studentNo", student.studentNo);
   const result = await StudentApi.insert(student) as Student
-  if (result.role == 100) {
-
-  } else {
-
-  }
+  studentId.value = result.id
 };
 
 // 文件上传
-const pcapfiles = ref<any[]>([]);
 const csvfiles = ref<any[]>([]);
 const fileid = ref<string>("");
 const disable = ref<boolean>(false)
@@ -151,13 +111,13 @@ const onRemove = (_uploadFile: UploadFile, _uploadFiles: UploadFiles) => {
     disable.value = false;
 }
 
-// 定义 FormParam 类，包含 id 和 isdeleted 属性
+// 定义 FormParam 类
 class FormParam {
   id: number = 0;
   isdeleted: boolean = false;
 }
 
-// 定义 Student2Resource 类，继承自 FormParam，并添加特定属性
+// 定义 Student2Resource 类
 class Student2Resource extends FormParam {
   studentId: number;
   sceneId: number;
@@ -181,7 +141,7 @@ class Student2Resource extends FormParam {
   }
 }
 
-// 定义 QueryParam 类，包含 condition、offset 和 limit 属性
+// 定义 QueryParam 类
 class QueryParam<T> {
   condition: T;
   offset: number = 0;
@@ -191,14 +151,16 @@ class QueryParam<T> {
     this.condition = condition;
   }
 }
+
+// 文件下载
 const downPcap = async () =>{
-    const studentCondition = new Student2Resource(31, 40003, '', '', Date.now());
-    const student2resource = new QueryParam(studentCondition)
-    const result= await Student2ResourceApi.query(student2resource) as Student2Resource[]
-    const results2r = result[0];
-    const segments = results2r.path.split('/')
-    const bucketstr = segments[0]
-    const pathstr = '/' + segments.slice(1).join('/')
+  const studentCondition = new Student2Resource(studentId.value, 40003, '', '', Date.now());
+  const student2resource = new QueryParam(studentCondition)
+  const result= await Student2ResourceApi.query(student2resource) as Student2Resource[]
+  const results2r = result[0];
+  const segments = results2r.path.split('/')
+  const bucketstr = segments[0]
+  const pathstr = '/' + segments.slice(1).join('/')
 
   const downfiles = ref([
     {
@@ -242,7 +204,22 @@ const downPcap = async () =>{
   }
 };
 
+// 弹出Dify工具
+const dialogVisible = ref(false);
+const question = async () => {
+  dialogVisible.value = true;
+}
+const handleClose = (done) => {
+  ElMessageBox.confirm('确定要关闭吗？')
+      .then(() => {
+        done(); // 关闭对话框
+      })
+      .catch(() => {
+        // 用户取消关闭
+      });
+};
 
+// 评估
 const score = ref(0.0);
 const scoreMessage = ref("");
 const scoreResultClass = ref("");
@@ -269,7 +246,7 @@ const scoreCsv = async () => {
 
   try {
     // 调用评分API
-    const commit = new Commit(0,31, 40003, 0, `temporary/${csvfiles.value[0]?.name}`, getCurrentTime(), false);
+    const commit = new Commit(0,studentId.value, 40003, 0, `temporary/${csvfiles.value[0]?.name}`, getCurrentTime(), false);
     // const commit = new Commit(0,111111, 40003, 0, `/home/xieyuqi/resultTest.csv`, getCurrentTime(), false);
     // const result = new Commit(0,111, 111, 65, 152000, false);
     const result = await ScoreApi.insert(commit) as Commit;
@@ -296,14 +273,6 @@ const scoreCsv = async () => {
     console.error('评分出错:', error);
   }
 };
-
-// 当前的特征值
-const feature = ref<any>({});
-const resultDialogVisible = ref<boolean>(false);
-const onClick = (_index: number, _row: any) => {
-    feature.value = _row;
-    resultDialogVisible.value = true;
-}
 
 </script>
 
@@ -344,19 +313,7 @@ const onClick = (_index: number, _row: any) => {
     margin-right: 10px;
 }
 
-.experiment-table {
-    width: 98%;
-    border-radius: 1%;
-    display: flex;
-    justify-content: center;
-    margin-left: 1%;
-    margin-right: 1%;
-
-    font-size: medium;
-
-}
-
-.experiment-download-upload {
+.experiment-download {
     display: flex;
     align-items: center;
     margin-top: 1%;
@@ -369,16 +326,6 @@ const onClick = (_index: number, _row: any) => {
 .experiment-download-label{
   font-size: 16px;
   color: gray;
-}
-
-.experiment-upload-label {
-    font-size: 16px;
-    color: gray;
-}
-
-.experiment-upload-btn {
-    display: flex;
-    align-items: center;
 }
 
 .experiment-qa {
@@ -439,4 +386,16 @@ const onClick = (_index: number, _row: any) => {
   }
 }
 
+.feishu {
+  width: 98%;
+  height: 100%;
+  border-radius: 1%;
+  display: flex;
+  justify-content: center;
+  margin-left: 1%;
+  margin-right: 1%;
+
+  font-size: medium;
+
+}
 </style>
