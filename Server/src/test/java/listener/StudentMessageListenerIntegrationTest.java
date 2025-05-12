dd52e5f3ff2b84d9ca78cda34b96042617394e44
gzy <815953227@qq.com>
2025-05-12 11:30:16 +0800

    修改了listener从minio中分发作业的逻辑

--- Server/src/test/java/listener/StudentMessageListenerIntegrationTest.java
+++ Server/src/test/java/listener/StudentMessageListenerIntegrationTest.java
@@ -8,6 +8,8 @@ import com.coldwindx.server.service.impl.StudentServiceImpl;
 import org.checkerframework.checker.signature.qual.PolySignature;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,16 +18,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class StudentMessageListenerIntegrationTest {
     @Autowired
     private StudentMessageListener studentMessageListener;
-
+    @Autowired
+    private StudentServiceImpl studentServiceImpl;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
     @Test
-    public void testRegistration() {
+    public void testRegistration() throws InterruptedException {
         Student student = new Student();
-        student.setId(123213L);
-        student.setStudentNo("1234567");
+        student.setId(1L);
+        student.setStudentNo("1");
         student.setName("name1");
         student.setRole(100);
         student.setGrade(0);
-        studentMessageListener.registration(student);
+//        studentServiceImpl.insert(student);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+//        System.out.println("12345");
+        student.setId(2L);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+        student.setId(3L);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+        student.setId(4L);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+        student.setId(5L);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+        student.setId(6L);
+        rabbitTemplate.convertAndSend("ex_student", "registed_test", student);
+//        Thread.sleep(10000);
+//        studentServiceImpl.insert(student);
+        System.out.println("Main thread done sending.");
+//        studentMessageListener.registration(student);
     }
 
 }
